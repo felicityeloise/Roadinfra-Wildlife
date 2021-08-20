c10d93b3357c7253af5719db261ec3686b8a163b
FelicityC-UQ <83437694+FelicityC-UQ@users.noreply.github.com>
2021-08-20 15:45:37 +1000

    Delete Functions directory

--- Functions/Felicity_Functions.R
+++
@@ -1,6 +0,0 @@
-tidy.df <- function(data.set){
-  # data.set is the user entry dataset to tidy up
-  data.set <- droplevels(data.set)
-  row.names(data.set) <- 1:nrow(data.set)
-  return(data.set)
-} # Close tidy.df
